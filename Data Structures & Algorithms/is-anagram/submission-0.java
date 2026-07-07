629e22f5b2aabf9d23217dd8a12cc3c6f1b2a15b
Joe Isaac <65372542+Joe-Isaac@users.noreply.github.com>
2026-07-07 04:40:12 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null){
+            return false;
+        }
+
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+
+        String sortedS = new String(sc);
+        String sortedT = new String(tc);
+
+        if(sortedS.equalsIgnoreCase(sortedT)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
